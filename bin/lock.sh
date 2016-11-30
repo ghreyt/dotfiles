e1721c7da41aac2810de453c9e89c5793c3a74bf
ghreyt <ghreyt@gmail.com>
2016-11-30 12:40:52 +0900

    touched for dell 7000

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-/usr/bin/i3lock-fancy -g -p -f "Noto-Sans" -- maim
+NAME=$(basename $0)
+
+if [[ $(pgrep -f $NAME) ]]; then
+    echo '$0:already locked'
+else
+    /usr/bin/i3lock-fancy -g -p -f "Noto-Sans" -- maim
+fi
