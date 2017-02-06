aa0ecde77dfaef3ce8dac817234d1606bfa6aa69
ghreyt <ghreyt@gmail.com>
2017-02-06 18:34:15 +0900

    bin:
        - added brightness_control.sh

--- bin/brightness_control.sh
+++ bin/brightness_control.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
-curr=$(xbacklight -get)
-echo $curr
+function usage {
+    echo ""
+    echo "usage> $0 <diff>"
+    echo ""
+    echo "- diff: [+|-]NUMBER" 
+    echo ""
+}
+
+if [[ $# != 1 ]]; then
+    usage
+    exit 1
+fi
+
+# init
+XBACKLIGHT=$(which xbacklight)
+DIFF=$1
+
+if [[ ! $XBACKLIGHT ]]; then
+    exit 1
+fi
+
+$XBACKLIGHT $1
+$XBACKLIGHT -get | cut -d'.' -f1
